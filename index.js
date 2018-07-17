38b67f44509b139b80f2121275d485413fc025f5
Alyssa Vongries <alyssavongries@gmail.com>
2018-07-17 01:52:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,17 +14,6 @@ function nowServing(katzDeliLine) {
 }
 }
 
-function currentLine(katzDeliLine) {
-  var line = []
-  for(let i = 0; i <= katzDeliLine.length ; i++)
-  if (katzDeliLine.length === 0) {
-    return "The line is currently empty.";
-  } else { 
-   line = [...line, ` ${i+1}. ${katzDeliLine[i]}`];
-   line.pop()
-}
-return `The line is currently: ${line}`
-}
 
 function currentLine(katzDeliLine) {
   if (katzDeliLine.length === 0) {
